41b6a3027e3916589bb6d8594514ee40242bec4a
sabrinahubb <60670666+sabrinahubb@users.noreply.github.com>
2023-11-29 15:25:35 -0600

    movements

---
+++ frontend/app.js
@@ -0,0 +1,21 @@
+const accountStatus = localStorage.getItem("account");
+const loginButtonsContainer = document.getElementById("loginButtons");
+
+if (accountStatus === "logged") {
+  const logoutButton = document.createElement("button");
+  logoutButton.textContent = "Logout";
+  logoutButton.className = "loginButton";
+  logoutButton.addEventListener("click", function () {
+    localStorage.removeItem("account");
+    window.location.href = "../../javlaba/backend/login/login.html";
+  });
+  loginButtonsContainer.appendChild(logoutButton);
+} else {
+  const loginButton = document.createElement("button");
+  loginButton.textContent = "Login";
+  loginButton.className = "loginButton";
+  loginButton.addEventListener("click", function () {
+    window.location.href = "../../javlaba/backend/login/login.html";
+  });
+  loginButtonsContainer.appendChild(loginButton);
+}
