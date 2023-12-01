7001bd36647771336fa67d861b7ac74bcdfda5ae
sabrinahubb <60670666+sabrinahubb@users.noreply.github.com>
2023-12-01 05:16:10 -0600

    updates

--- frontend/app.js
+++ frontend/app.js
@@ -19,3 +19,24 @@ if (accountStatus === "logged") {
   });
   loginButtonsContainer.appendChild(loginButton);
 }
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDuDEAYmSZfMxD5DCd8Lao-XINbE4J0SO0",
+  authDomain: "sdlab3-f5603.firebaseapp.com",
+  projectId: "sdlab3-f5603",
+  storageBucket: "sdlab3-f5603.appspot.com",
+  messagingSenderId: "868353866547",
+  appId: "1:868353866547:web:2168c0b07ce400efc69c46",
+  measurementId: "G-KCH55E3QRJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- frontend/backend/server/server.js
+++ frontend/backend/server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+f
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3000;
